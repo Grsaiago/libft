415cf88b859e6cdf45ba616805dfb882f029f353
gsaiago <gsaiago@student.42.rio>
2023-04-07 14:55:45 -0300

    Add(strlen): Protection against null

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: gsaiago <gsaiago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 19:12:34 by gsaiago           #+#    #+#             */
-/*   Updated: 2022/06/01 14:11:50 by gsaiago          ###   ########.fr       */
+/*   Updated: 2023/04/07 14:55:37 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *c)
 {
 	size_t	i;
 
+	if (!c)
+		return (0);
 	i = 0;
 	while (c[i])
 		i++;
